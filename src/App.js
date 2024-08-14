e087e82ae890fce2e7d4901c1416ff7c38234d6c
Uttam Rao <uttam.rao@nihilent.com>
2024-08-14 17:02:10 +0530

    database config

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
+import { useState } from 'react';
 import './App.css';
-import { auth, provider, signInWithPopup } from './firebase';
+import { auth, db, provider, ref, set, signInWithPopup } from './firebase';
 
 function App() {
+  const [userData, setUserData] = useState()
+  console.log(userData)
+
   const handleSignIn = async () => {
     try {
       const res = await signInWithPopup(auth, provider);
       // User signed in successfully
-      console.log(res)
+      setUserData(res.user)
     } catch (error) {
       console.error('Error signing in with Google:', error.message);
     }
   };
+
+  const handleData = async () => {
+    const uid = userData.uid; // Make sure this is a valid UID
+    if (uid) {
+        await set(ref(db, 'users/' + uid), {
+            name: userData.displayName,
+            email: userData.email,
+        });
+    } else {
+        console.error('UID is missing or invalid');
+    }
+}
+
   return (
     <div className="App">
       <button onClick={handleSignIn}>
-      Sign In with Google
-    </button>
+        Sign In with Google
+      </button>
+      <button onClick={handleData}>Send Data to Database</button>
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "firebase/auth";
+import { getDatabase, ref, set, get, child, update } from "firebase/database"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,13 +15,15 @@ const firebaseConfig = {
   storageBucket: "todo-8b40c.appspot.com",
   messagingSenderId: "846622512297",
   appId: "1:846622512297:web:03d082bdde8b112e3885b8",
-  measurementId: "G-X6R7LC9GES"
+  measurementId: "G-X6R7LC9GES",
+  databaseURL: "https://todo-8b40c-default-rtdb.asia-southeast1.firebasedatabase.app",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
+const db = getDatabase(app)
 const analytics = getAnalytics(app);
 
-export { auth, provider, signInWithPopup, signOut, onAuthStateChanged };
+export { auth, provider, signInWithPopup, signOut, onAuthStateChanged, db, ref, set, get, child, update };
